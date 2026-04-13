1e6753695d4196e35422872353caed7f3a9df432
dimakudrynskyi <60889475+dimakudrynskyi@users.noreply.github.com>
2026-04-13 17:20:46 +0300

    feat: Add CloudWatch log group and Fluent Bit DaemonSet configuration

--- variables.tf
+++ variables.tf
@@ -1,84 +1,22 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
-variable "project_name" {
-  description = "Name of the project"
+variable "cluster_endpoint" {
+  description = "Kubernetes cluster endpoint"
   type        = string
-  default     = "travel-site"
 }
 
-variable "environment" {
-  description = "Deployment environment (dev, staging, prod)"
-  type        = string
-  default     = "dev"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "eks_cluster_version" {
-  description = "Kubernetes version for EKS cluster"
-  type        = string
-  default     = "1.29"
-}
-
-variable "eks_node_instance_type" {
-  description = "EC2 instance type for EKS worker nodes"
-  type        = string
-  default     = "t3.medium"
-}
-
-variable "eks_node_min_size" {
-  description = "Minimum number of EKS worker nodes"
+variable "cloudwatch_log_retention_days" {
+  description = "CloudWatch log retention in days"
   type        = number
-  default     = 2
-}
-
-variable "eks_node_max_size" {
-  description = "Maximum number of EKS worker nodes"
-  type        = number
-  default     = 5
-}
-
-variable "eks_node_desired_size" {
-  description = "Desired number of EKS worker nodes"
-  type        = number
-  default     = 2
-}
-
-variable "db_instance_class" {
-  description = "RDS instance class"
-  type        = string
-  default     = "db.t3.micro"
-}
-
-variable "db_name" {
-  description = "Name of the PostgreSQL database"
-  type        = string
-  default     = "travel_site"
-}
-
-variable "db_username" {
-  description = "Master username for the RDS instance"
-  type        = string
-  default     = "dbadmin"
-  sensitive   = true
-}
-
-variable "db_password" {
-  description = "Master password for the RDS instance"
-  type        = string
-  sensitive   = true
-}
-
-variable "cloudfront_oai_arn" {
-  description = "CloudFront Origin Access Identity ARN for S3 bucket policy (placeholder)"
-  type        = string
-  default     = ""
+  default     = 7
 }
